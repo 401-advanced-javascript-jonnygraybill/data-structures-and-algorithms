856d0071f1700dc1111ebe40781bfd642bf4c18c
Jonny Graybill <jon.a.graybill@gmail.com>
2019-08-13 20:19:37 -0700

    fizz-tree

---
+++ fizzBuzzTree/__tests__/fizz-buzz-tree.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const BinaryTree = require('../../tree/tree.js');
+const fizzBuzz = require('../fizz-buzz-tree.js');
+const Node = require('../node.js');
+
+describe('Tree Data Structure Tests', () => {
+
+  it('Can do the fizzBuzz', () => {
+    tree = new BinaryTree()
+    console.log(tree);
+  });
+});
